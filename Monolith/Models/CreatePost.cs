15c733811b978caa6745017853b6b0e7f4b4d15f
Lost1nC0d3 <L0st1nC0d3@users.noreply.github.com>
2022-04-11 16:11:03 +0200

    Add files via upload

---
+++ Monolith/Models/CreatePost.cs
@@ -0,0 +1,13 @@
+﻿namespace Monolith.Models
+{
+    public class CreatePost : EditPost
+    {
+        public int AuthorId { get; set; }
+    }
+
+    public class EditPost
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
